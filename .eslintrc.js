2b35ff9127094f8388a8d7db7308b80c6cca66db
Leo Jacobs <leo@the-earthworks.com>
2018-09-12 11:06:31 +0100

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": ["airbnb-base", "prettier"],
+  "rules": {
+    "no-console": "off",
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"], // Specific for windows
+    "no-underscore-dangle": "off"
+  }
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+// This is a sample file. Delete the contents of this file after you cloned the repo.
+
+/* Sample Function => Delete all the lines below */
+const add = (a, b) => a + b
+add(3, 4)
